af603f5b1a7490d1e862c7a0441d909ad9094f8e
Overlord-12 <danya.argastesv.02@mail.ru>
2021-05-10 11:13:24 +0300

    Добавьте файлы проекта.

---
+++ OssApp(Xamarin Forms)/OssApp(Xamarin Forms)/View/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace OssApp_Xamarin_Forms_
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OssApp(Xamarin Forms)/OssApp(Xamarin Forms)/ViewModel/Registr.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OssApp_Xamarin_Forms.ViewModel
+{
+    class Registr
+    {
+
+    }
+}
